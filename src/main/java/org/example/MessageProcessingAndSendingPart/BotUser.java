70544109d46360b9df5af2c4bdab56c85f406424
ArtemPoliakov <apoliakov50@gmail.com>
2023-10-24 21:33:19 +0300

    User finished, sending logics in sender to be done))

--- src/main/java/org/example/MessageProcessingAndSendingPart/BotUser.java
+++ src/main/java/org/example/MessageProcessingAndSendingPart/BotUser.java
@@ -3,6 +3,9 @@ package org.example.MessageProcessingAndSendingPart;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
+import org.example.app.Client;
+import org.example.bank.Bank;
+import org.example.bank.Currency;
 
 import javax.management.timer.Timer;
 import java.time.*;
@@ -10,6 +13,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
+import static org.example.bank.Bank.BankName.*;
+import static org.example.bank.Currency.CurrencyName.EUR;
+import static org.example.bank.Currency.CurrencyName.USD;
 
 @Data
 public class BotUser {
@@ -19,50 +26,61 @@ public class BotUser {
     private static final boolean defaultMonobankSetting = false;
     private static final boolean defaultPrivatBankSetting = true;
     private static final boolean defaultNbuSetting = false;
-    private static final boolean usdSetting = true;
-    private static final boolean eurSetting = false;
+    private static final boolean defaultUsdSetting = true;
+    private static final boolean defaultEurSetting = true;
 
     private long userChatId;
-    private int timeOfSending = DEFAULT_TIME_OF_SENDING;
-    private int timeZone = DEFAULT_TIME_ZONE;
-    private int signsAfterComma = MAX_SIGNS_AFTER_COMMA;
-
-//    private Map<BankNames, BotUser> banksMap;
-//    private Map<CurrencyNames, BotUser> currenciesMap;
-
-//    private boolean isMonoBankNeeded;
-//    private boolean isPrivatBankNeeded;
-//    private boolean isNbuNeeded;
-//    private boolean isUsdNeeded;
-//    private boolean isEurNeeded;
-
+    private int timeOfSending;
+    private int timeZone;
+    private int signsAfterComma;
+    private Map<Bank.BankName, Boolean> banksMap;
+    private Map<Currency.CurrencyName, Boolean> currenciesMap;
     @Setter(value = AccessLevel.NONE)
     private ScheduledThreadPoolExecutor executor;
     private BotUser(long userChatId){
         this.userChatId = userChatId;
-        //TODO: do maps setting here when Egor's part arrives
     }
-    public BotUser newDefaultUserById(long chatId){
-        return new BotUser(chatId);
+    public static BotUser newDefaultUserById(long chatId){
+        BotUser botUser = new BotUser(chatId);
+        botUser.setBanksMap(new HashMap<>());
+        botUser.setCurrenciesMap(new HashMap<>());
+        botUser.signsAfterComma = MAX_SIGNS_AFTER_COMMA;
+        botUser.timeZone = DEFAULT_TIME_ZONE;
+        botUser.timeOfSending = DEFAULT_TIME_OF_SENDING;
+        botUser.banksMap.put(MONO,defaultMonobankSetting);
+        botUser.banksMap.put(PRIVAT, defaultPrivatBankSetting);
+        botUser.banksMap.put(NBU, defaultNbuSetting);
+        botUser.currenciesMap.put(EUR, defaultEurSetting);
+        botUser.currenciesMap.put(USD, defaultUsdSetting);
+        return botUser;
     }
 
     public void setTimeOfSending(int timeOfSending){
         this.timeOfSending = timeOfSending;
-        if(executor!=null){
-            if(!executor.isShutdown()){
-                executor.shutdownNow();
-            }
-        }
+        killUserSendingProcess(executor);
         invokeSendingProcess();
     }
     public void setTimeZone(int timeZone){
         this.timeZone = timeZone;
+        if(executor.isShutdown()){
+            return;
+        }else {
+            killUserSendingProcess(executor);
+            invokeSendingProcess();
+        }
+    }
+    public void setBankNeedByName(Bank.BankName name, Boolean need){
+        banksMap.put(name, need);
+    }
+    public void setCurrencyNeedByName(Currency.CurrencyName name, Boolean need){
+        currenciesMap.put(name, need);
+    }
+    public void killUserSendingProcess(ScheduledThreadPoolExecutor executor){
         if(executor!=null){
             if(!executor.isShutdown()){
                 executor.shutdownNow();
             }
         }
-        invokeSendingProcess();
     }
     public void invokeSendingProcess(){
         long from = OffsetDateTime.now(ZoneOffset.ofHours(timeZone)).toInstant().toEpochMilli();
@@ -76,7 +94,7 @@ public class BotUser {
         executor.scheduleAtFixedRate(()->{
             BotMessageProcessor processor = new BotMessageProcessor();
             //TODO: add a param with Bank info DTO when it is added
-            processor.sendMessageToUser(this);
+            processor.sendMessageToUser(this, Client.getInstance().getBanks(), 1);
         },approximateDiff, Timer.ONE_DAY, TimeUnit.MILLISECONDS);
     }
 }
