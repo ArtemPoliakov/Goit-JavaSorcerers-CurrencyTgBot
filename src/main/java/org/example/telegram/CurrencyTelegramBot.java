2e2fad2d67bf730eaebf4efa5b9e0d97603a991c
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2023-10-24 21:52:13 +0300

    Update CurrencyTelegramBot.java
    Adding the "/help" command and processing valid messages from the user

--- src/main/java/org/example/telegram/CurrencyTelegramBot.java
+++ src/main/java/org/example/telegram/CurrencyTelegramBot.java
@@ -3,23 +3,38 @@ package org.example.telegram;
 import lombok.SneakyThrows;
 import org.example.command.*;
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.regex.Pattern;
 
 import static org.example.telegram.BotConstants.BOT_NAME;
 import static org.example.telegram.BotConstants.BOT_TOKEN;
 
 public class CurrencyTelegramBot extends TelegramLongPollingCommandBot {
+    private final Pattern commandPattern = Pattern.compile("/\\w+");
 
     public CurrencyTelegramBot() {
+        register(new HelpCommand());
         register(new AlertTimes());
         register(new BackCommand());
         register(new BankCommand());
         register(new CurrencyCommand());
         register(new DecimalPlaces());
-        register(new HelpCommand());
         register(new SettingsCommand());
         register(new StartCommand());
+        registerDefaultAction((absSender, message) -> {
+            SendMessage responseMessage = new SendMessage();
+            responseMessage.setText("Ви ввели команду який бот не може розпізнати🤷🏼‍♂️\n" + "Це бот знає ось такі команди: \n" + "/start ~ /help");
+            responseMessage.setChatId(message.getChatId());
+            try {
+                absSender.execute(responseMessage);
+            } catch (TelegramApiException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     @Override
@@ -35,6 +50,7 @@ public class CurrencyTelegramBot extends TelegramLongPollingCommandBot {
     @Override
     @SneakyThrows
     public void processNonCommandUpdate(Update update) {
+        System.out.println("Received update: " + update.toString());
         if (update.hasCallbackQuery()) {
             CallbackQuery callbackQuery = update.getCallbackQuery();
             String action = callbackQuery.getData();
@@ -42,7 +58,8 @@ public class CurrencyTelegramBot extends TelegramLongPollingCommandBot {
                 SettingsCommand settingsCommand = new SettingsCommand();
                 settingsCommand.execute(this, callbackQuery.getFrom(), callbackQuery.getMessage().getChat(), null);
             } else if ("back".equals(action)) {
-                System.out.println("---");
+                BankCommand bankCommand = new BankCommand();
+                bankCommand.execute(this, callbackQuery.getFrom(), callbackQuery.getMessage().getChat(), null);
             } else if ("decimal places".equals(action)) {
                 DecimalPlaces decimalPlacesCommand = new DecimalPlaces();
                 decimalPlacesCommand.execute(this, callbackQuery.getFrom(), callbackQuery.getMessage().getChat(), null);
@@ -53,15 +70,23 @@ public class CurrencyTelegramBot extends TelegramLongPollingCommandBot {
                 AlertTimes alertTimesCommand = new AlertTimes();
                 alertTimesCommand.execute(this, callbackQuery.getFrom(), callbackQuery.getMessage().getChat(), null);
             }
-
         }
+        commandHelp(update);
+    }
+
+    private void commandHelp(Update update) throws TelegramApiException {
         if (update.hasMessage() && update.getMessage().hasText()) {
             String message = update.getMessage().getText();
             if ("/help".equals(message)) {
                 HelpCommand helpCommand = new HelpCommand();
                 helpCommand.execute(this, update.getMessage().getFrom(), update.getMessage().getChat(), null);
             }
+            if (!commandPattern.matcher(message).matches()) {
+                SendMessage responseMessage = new SendMessage();
+                responseMessage.setText("Ви ввели текст який бот не може розпізнати🤷🏼‍♂️\n" + "Це бот знає ось такі команди: \n" + "/start ~ /help");
+                responseMessage.setChatId(update.getMessage().getChatId());
+                execute(responseMessage);
+            }
         }
     }
 }
-
