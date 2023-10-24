415205935a24182fc2719f0cbe255750136579e8
ArtemPoliakov <apoliakov50@gmail.com>
2023-10-24 14:33:30 +0300

    Pre-changes in user, not functional code

--- src/main/java/org/example/MessageProcessingAndSendingPart/BotUser.java
+++ src/main/java/org/example/MessageProcessingAndSendingPart/BotUser.java
@@ -5,30 +5,58 @@ import lombok.Data;
 import lombok.Setter;
 
 import javax.management.timer.Timer;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
+import java.time.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 @Data
 public class BotUser {
+    private static final int MAX_SIGNS_AFTER_COMMA = 4;
+    private static final int DEFAULT_TIME_OF_SENDING = 12;
+    private static final int DEFAULT_TIME_ZONE = 0;
+    private static final boolean defaultMonobankSetting = false;
+    private static final boolean defaultPrivatBankSetting = true;
+    private static final boolean defaultNbuSetting = false;
+    private static final boolean usdSetting = true;
+    private static final boolean eurSetting = false;
+
     private long userChatId;
-    private int signsAfterComma;
-    private boolean isMonoBankNeeded;
-    private boolean isPrivatBankNeeded;
-    private boolean isNbuNeeded;
-    private boolean isUsdNeeded;
-    private boolean isEurNeeded;
-    private int timeOfSending;
-    private int timeZone;
+    private int timeOfSending = DEFAULT_TIME_OF_SENDING;
+    private int timeZone = DEFAULT_TIME_ZONE;
+    private int signsAfterComma = MAX_SIGNS_AFTER_COMMA;
+
+//    private Map<BankNames, BotUser> banksMap;
+//    private Map<CurrencyNames, BotUser> currenciesMap;
+
+//    private boolean isMonoBankNeeded;
+//    private boolean isPrivatBankNeeded;
+//    private boolean isNbuNeeded;
+//    private boolean isUsdNeeded;
+//    private boolean isEurNeeded;
 
     @Setter(value = AccessLevel.NONE)
     private ScheduledThreadPoolExecutor executor;
+    private BotUser(long userChatId){
+        this.userChatId = userChatId;
+        //TODO: do maps setting here when Egor's part arrives
+    }
+    public BotUser newDefaultUserById(long chatId){
+        return new BotUser(chatId);
+    }
 
     public void setTimeOfSending(int timeOfSending){
         this.timeOfSending = timeOfSending;
+        if(executor!=null){
+            if(!executor.isShutdown()){
+                executor.shutdownNow();
+            }
+        }
+        invokeSendingProcess();
+    }
+    public void setTimeZone(int timeZone){
+        this.timeZone = timeZone;
         if(executor!=null){
             if(!executor.isShutdown()){
                 executor.shutdownNow();
