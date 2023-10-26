0b699cf01e01a6b4a95177e166c8759d2eca2a57
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2023-10-26 23:07:05 +0300

    Revert "Create BotConstants.java"
    This reverts commit 9fed96785dd0446a36d12dc0b1866361d71b3cc1.

--- src/main/java/org/example/telegram/BotConstants.java
+++
@@ -1,6 +0,0 @@
-package org.example.telegram;
-
-public class BotConstants {
-    public static final String BOT_NAME = "Exchange Rate Info Bot\uD83D\uDCB0";
-    public static final String BOT_TOKEN = "6962517461:AAHqzd7W-_oMYpud_W8m5prxSKVbFNXI16o";
-}
